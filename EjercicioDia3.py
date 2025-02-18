1b6b187b6009c0024a392e020981d2bced5b326f
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-02-18 09:13:55 -0600

    programa 4 y 5

--- EjercicioDia3.py
+++ EjercicioDia3.py
@@ -1,6 +1,4 @@
-
-
-## Write a script that prompts the user to enter base and 
+## 4. Write a script that prompts the user to enter base and 
 # height of the triangle and calculate an area of this 
 # triangle (area = 0.5 x b x h).
 
@@ -8,3 +6,13 @@ base = float(input("Ingresa la base del triangulo: "))
 altura = float(input("Ingresa la altura del triangulo: "))
 area = 0.5 * base * altura
 print ("La area del triangulo es de ", area)
+
+## 5.Write a script that prompts the user to enter side a, side b, and side c of 
+# the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+
+sideA = float(input("Ingresa la medida del lado A: "))
+sideB = float(input("Ingresa la medida del lado B: "))
+sideC = float(input("Ingresa la medida del lado C: "))
+perimetro = sideA + sideB + sideC
+print ("El perimetro del triangulo es de ", perimetro)
+
