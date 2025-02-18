cb1496c0e9248a45485c5cebdaee1dad215e843d
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-02-18 08:49:04 -0600

    programa 4

--- EjercicioDia3.py
+++ EjercicioDia3.py
@@ -4,5 +4,7 @@
 # height of the triangle and calculate an area of this 
 # triangle (area = 0.5 x b x h).
 
-base = float(input("ingresa la base del triangulo: "))
-
+base = float(input("Ingresa la base del triangulo: "))
+altura = float(input("Ingresa la altura del triangulo: "))
+area = 0.5 * base * altura
+print ("La area del triangulo es de ", area)
