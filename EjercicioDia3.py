911c5bf06c6deb90ea8756109329bf320fa16b80
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-02-18 08:36:58 -0600

    programa 4

---
+++ EjercicioDia3.py
@@ -0,0 +1,8 @@
+
+
+## Write a script that prompts the user to enter base and 
+# height of the triangle and calculate an area of this 
+# triangle (area = 0.5 x b x h).
+
+base = float(input("ingresa la base del triangulo: "))
+
