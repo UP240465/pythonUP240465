5cbee198eb43c2a79eeb245fd121a36f1a13bcf2
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-02-26 10:40:21 -0600

    dia4

---
+++ dia4.py
@@ -0,0 +1,47 @@
+##1.Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+print ( 'thirty' ' days' ' of' ' python')
+
+##2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+print ('coding' ' for' ' all')
+
+##3. Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding For All"
+
+##4. Print the variable company using _print()_.
+print(company)
+
+##5. Print the length of the company string using _len()_ method and _print()_.
+len_compamy=len(company)
+print(len_compamy)
+
+##6. Change all the characters to uppercase letters using _upper()_ method.
+
+
+##7. Change all the characters to lowercase letters using _lower()_ method.
+##8. Use capitalize(), title(), swapcase() methods to format the value of the string _Coding For All_.
+##9. Cut(slice) out the first word of _Coding For All_ string.
+##10. Check if _Coding For All_ string contains a word Coding using the method index, find or other methods.
+##11. Replace the word coding in the string 'Coding For All' to Python.
+##12. Change Python for Everyone to Python for All using the replace method or other methods.
+##13. Split the string 'Coding For All' using space as the separator (split()) .
+##14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+##15. What is the character at index 0 in the string _Coding For All_.
+##16. What is the last index of the string _Coding For All_.
+##18. Create an acronym or an abbreviation for the name 'Python For Everyone'.
+##19. Create an acronym or an abbreviation for the name 'Coding For All'.
+##20. Use index to determine the position of the first occurrence of C in Coding For All.
+##21. Use index to determine the position of the first occurrence of F in Coding For All.
+##22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
+##23. Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+##24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+##25. Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+##26. Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+##28. Does '\'Coding For All' start with a substring _Coding_?
+##29. Does 'Coding For All' end with a substring _coding_?
+##30. '&nbsp;&nbsp; Coding For All &nbsp;&nbsp;&nbsp; &nbsp;' &nbsp;, remove the left and right trailing spaces in the given string.
+##31.Which one of the following variables return True when we use the method isidentifier():
+##32.The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+##33.Use the new line escape sequence to separate the following sentences.
+##34. Use a tab escape sequence to write the following lines.
+##35.Use the string formatting method to display the following:
+##36.Make the following using string formatting methods:
