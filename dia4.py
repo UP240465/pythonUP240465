6ed8140ab9cc4b349857fd458bec5e9c1fd3cb70
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-02-27 11:52:31 -0600

    dia4

--- dia4.py
+++ dia4.py
@@ -15,20 +15,54 @@ len_compamy=len(company)
 print(len_compamy)
 
 ##6. Change all the characters to uppercase letters using _upper()_ method.
-print(company.upre())
+print(company.upper())
 
 ##7. Change all the characters to lowercase letters using _lower()_ method.
+print(company.lower())
+
 ##8. Use capitalize(), title(), swapcase() methods to format the value of the string _Coding For All_.
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
 ##9. Cut(slice) out the first word of _Coding For All_ string.
+print(company[0:6])
+
 ##10. Check if _Coding For All_ string contains a word Coding using the method index, find or other methods.
+print(company.index("Coding"))
+
 ##11. Replace the word coding in the string 'Coding For All' to Python.
+print(company.replace("Coding", "Python"))
+
 ##12. Change Python for Everyone to Python for All using the replace method or other methods.
+com="Python for Everyone"
+print(com.replace('Everyone', 'All'))
+
 ##13. Split the string 'Coding For All' using space as the separator (split()) .
+print(company.split())
+
 ##14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+t=("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon")
+print(t.split(","))
+
 ##15. What is the character at index 0 in the string _Coding For All_.
+print(company[0])
+
 ##16. What is the last index of the string _Coding For All_.
+print(company[-1])
+
+##17. What character is at index 10 in "Coding For All" string.
+print(company[10])
+
 ##18. Create an acronym or an abbreviation for the name 'Python For Everyone'.
+com= "Python For Everyone"
+acronym= com[0] + com[7] + com[11]
+print (acronym)
+
 ##19. Create an acronym or an abbreviation for the name 'Coding For All'.
+acr=company[0] + company[-7] + company[-3]
+print (acr)
+
 ##20. Use index to determine the position of the first occurrence of C in Coding For All.
 ##21. Use index to determine the position of the first occurrence of F in Coding For All.
 ##22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
