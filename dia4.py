c41cda395f6de043f65dfa66c36326fb04b7379f
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-03-04 09:31:51 -0600

    dia 4

--- dia4.py
+++ dia4.py
@@ -64,18 +64,78 @@ acr=company[0] + company[-7] + company[-3]
 print (acr)
 
 ##20. Use index to determine the position of the first occurrence of C in Coding For All.
+print (company.index ("C"))
+
 ##21. Use index to determine the position of the first occurrence of F in Coding For All.
+print (company.index ("F"))
+
 ##22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
+print (company.rfind ("l"))
+
 ##23. Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+oracion = 'You cannot end a sentence with because because because is a conjunction'
+print (oracion.index ("because"))
+
 ##24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+print (oracion.rindex ("because"))
+
 ##25. Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+palabra = "because"
+primerapalabra= oracion.index (palabra)
+ultimapalbra= oracion.rindex(palabra) + len(palabra)+1
+print (oracion)
+print(oracion[primerapalabra:ultimapalbra])
+
 ##26. Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+palabra = "because"
+print (oracion.index(palabra))
+
+##27.Slice out the phrase 'because because because' in the following sentence: ' You cannot end a sentence with because because because is a conjunction'
+palabra = "because"
+primerapalabra= oracion.index (palabra)
+ultimapalbra= oracion.rindex(palabra) + len(palabra)+1
+print (oracion)
+print(oracion[primerapalabra:ultimapalbra])
+
 ##28. Does '\'Coding For All' start with a substring _Coding_?
+print(company.startswith("Coding"))
+
 ##29. Does 'Coding For All' end with a substring _coding_?
+print(company.endswith ("coding"))
+
 ##30. '&nbsp;&nbsp; Coding For All &nbsp;&nbsp;&nbsp; &nbsp;' &nbsp;, remove the left and right trailing spaces in the given string.
+remove= "'&nbsp;&nbsp; Coding For All &nbsp;&nbsp;&nbsp; &nbsp;' &nbsp;"
+print (remove.strip(" &nbsp; "))
+
 ##31.Which one of the following variables return True when we use the method isidentifier():
+print ("30DaysOfPython".isidentifier())
+print ("thity_days_of_python".isidentifier())
+
 ##32.The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+names = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+resu= " #".join(names)
+print(resu)
+
 ##33.Use the new line escape sequence to separate the following sentences.
+print('I am enjoying this challenge. \nI just wonder what is next')
+
 ##34. Use a tab escape sequence to write the following lines.
+print ('Name\tAge\tCountry\tCity ')
+print("Asabeneh\t250\tFinland\tHelsinki ")
+
 ##35.Use the string formatting method to display the following:
+radio= 10
+area= 3.14 * radio ** 2
+f_s='The area of a circle with radius %d is %.2f meters square. '%(radio, area)
+print (f_s)
+
 ##36.Make the following using string formatting methods:
+a=8
+b=6
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b)) 
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
