7b123e00bb49d091c12909dc6f30424f0ce0ad43
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-03-11 09:32:38 -0600

    dia6

--- dia5.py
+++ dia5.py
@@ -110,3 +110,4 @@ full_stack = integrar
 full_stack.insert(5, 'Python')
 full_stack.insert(6, 'SQL')
 print (full_stack)
+
---
+++ dia6.py
@@ -0,0 +1,39 @@
+### Exercises: Level 1
+
+##1. Create an empty tuple
+print(tuple())
+
+##2. Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+hermanos=('Kevin', 'Paulo', 'Yazid'. split())
+hermanas=('Yajaira', 'Frida', 'Gina'. split())
+
+##3. Join brothers and sisters tuples and assign it to siblings
+familia= hermanos+hermanas
+print (familia)
+
+##4. How many siblings do you have?
+print (len(familia))
+
+##5. Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+padres = ('Georgina', 'Raudel')
+family_members= familia + padres
+print (family_members)
+
+### Exercises: Level 2
+
+##1. Unpack siblings and parents from family_members
+
+##2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+
+##3. Change the about food_stuff_tp  tuple to a food_stuff_lt list
+
+##4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+
+##5. Slice out the first three items and the last three items from food_staff_lt list
+
+##6. Delete the food_staff_tp tuple completely
+
+##7. Check if an item exists in  tuple:
+
+##- Check if 'Estonia' is a nordic country
+##- Check if 'Iceland' is a nordic country##
