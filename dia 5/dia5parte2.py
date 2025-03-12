433b1df4f87bf5b5d1c914bb8d785dd73c876451
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-03-12 10:35:20 -0600

    dia 5 parte 2

---
+++ dia 5/dia5parte2.py
@@ -0,0 +1,37 @@
+### Exercises: Level 2
+
+#1 The following is a list of 10 students ages:
+
+#```sh
+#ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+#```
+
+#- Sort the list and find the min and max age
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+age_en_orden=sorted(ages)
+print(age_en_orden)
+minimo=min(age_en_orden)
+maximo=max(age_en_orden)
+print(minimo, maximo)
+# Add the min age and the max age again to the list
+age_en_orden.insert(0, age_en_orden[0])
+age_en_orden.insert(-1, age_en_orden[-1])
+print(age_en_orden)
+
+# Find the median age (one middle item or two middle items divided by two)
+medio=len(age_en_orden)//2
+print(age_en_orden[medio])
+# Find the average age (sum of all items divided by their number )
+promedio=sum(age_en_orden)//(len(age_en_orden))
+print(promedio)
+# Find the range of the ages (max minus min)
+rango=max(age_en_orden)-(min(age_en_orden))
+print (rango)
+
+# Compare the value of (min - average) and (max - average), use _abs()_ method
+valor1=min(age_en_orden)-(promedio)
+valor2=max(age_en_orden)-(promedio)
+print(abs(valor1-valor2))
+#. Find the middle country(ies) in the [countries list](https://github.com/Asabeneh/30-Days-Of-Python/tree/master/data/countries.py)
+#. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+#. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
--- dia6.py
+++ dia6.py
@@ -22,12 +22,23 @@ print (family_members)
 ### Exercises: Level 2
 
 ##1. Unpack siblings and parents from family_members
+family_members=list(family_members)
+print (family_members)
+siblings= (family_members[0:5])
+print('Mis hermanos', siblings)
+sibling = (family_members[6:])
+print ('Mis papas son ',sibling)
 
 ##2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+food_stuff_tp= ('manzana', 'mango', 'platano', 'papa', 'calabaza', 'chayote', 'carne', 'leche', 'huevo')
+print (food_stuff_tp)
 
 ##3. Change the about food_stuff_tp  tuple to a food_stuff_lt list
+lista=list(food_stuff_tp)
+print (lista)
 
 ##4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+
 
 ##5. Slice out the first three items and the last three items from food_staff_lt list
 
