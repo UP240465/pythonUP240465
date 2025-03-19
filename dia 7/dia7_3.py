85518e85356825cadaa1f90af58bea9ae9c597ae
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-03-19 09:59:51 -0600

    dia7

--- dia 7/dia7_3.py
+++ dia 7/dia7_3.py
@@ -11,4 +11,13 @@ else:
     print('el mas largo es: ', len(ageset))
 
 #1. Explain the difference between the following data types: string, list, tuple and set
+print('Una lista es una estructura de datos no homogénea que almacena los elementos en columnas de una sola fila o de varias filas.')
+print('Una tupla es una estructura de datos no homogénea que almacena elementos en columnas de una sola fila o de varias filas.')
+print('La estructura de datos establecida no es homogénea, pero almacena los elementos en una sola fila.')
+print('Un diccionario también es una estructura de datos no homogénea que almacena pares clave-valor.')
+
 #2. _I am a teacher and I love to inspire and teach people._ How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
+o='I am a teacher and I love to inspire and teach people'
+oracion=('I am a teacher and I love to inspire and teach people'.split())
+set_oracion= set(oracion)
+print('Ahi hay ', len(set_oracion), 'Las unicas palabras en la oracion: ', o)
