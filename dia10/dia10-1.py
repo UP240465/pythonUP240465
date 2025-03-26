cc445f1697ca1b5d3162069412761508c87dc21e
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-03-26 10:03:53 -0600

    dia10

---
+++ dia10/dia10-1.py
@@ -0,0 +1,63 @@
+##1. Iterate 0 to 10 using for loop, do the same using while loop.
+for i in range (11):
+    print(i)
+
+i = 0
+while i < 11:
+    print(i)
+    i += 1
+
+
+##2. Iterate 10 to 0 using for loop, do the same using while loop.
+i = 0
+while i < 11:
+    print(i)
+    i += 1
+
+#3. Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+
+  # ```py
+     #
+     ##
+     ###
+     ####
+     #####
+     ######
+     #######
+   #```
+car = '#'
+for i in range (8):
+    print(car*i)
+
+#4. Use nested loops to create the following:
+
+   #```sh
+   # # # # # # # #
+   # # # # # # # #
+   # # # # # # # #
+   # # # # # # # #
+   # # # # # # # #
+   # # # # # # # #
+   # # # # # # # #
+   # # # # # # # #
+   #```
+
+##5. Print the following pattern:
+
+   #```sh
+   #0 x 0 = 0
+   #1 x 1 = 1
+   #2 x 2 = 4
+   #3 x 3 = 9
+   #4 x 4 = 16
+   #5 x 5 = 25
+   #6 x 6 = 36
+   #7 x 7 = 49
+   #8 x 8 = 64
+   #9 x 9 = 81
+   #10 x 10 = 100
+   #```
+
+##6. Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+#7. Use for loop to iterate from 0 to 100 and print only even numbers
+#8. Use for loop to iterate from 0 to 100 and print only odd numbers
