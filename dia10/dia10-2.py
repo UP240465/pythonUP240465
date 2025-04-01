8c1f3e8c6e349007b4e07a3eab3cd76bd89a1725
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-04-01 09:33:46 -0600

    dia10

---
+++ dia10/dia10-2.py
@@ -0,0 +1,26 @@
+### Exercises: Level 2
+    
+1.  #Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+
+'''sh
+   The sum of all numbers is 5050.
+   '''
+n=0
+for i in range (101):
+    n += i
+    print(n)
+
+
+1. #Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+'''sh
+    The sum of all evens is 2550. And the sum of all odds is 2500.
+    '''
+sumapar=0
+sumaimpar=0
+for i in range (101):
+    if i % 2 ==0:
+        sumapar+=i
+    else:
+        sumaimpar+=i
+print('la suma par es de ', sumapar)
+print('la suma impar es de ',sumaimpar)
