5839d105e60b8f7854bcf69ffcea1f82f1905eee
UP240465 <UP240465@alumnos.upa.edu.mx>
2025-04-04 22:30:53 -0600

    dia15

---
+++ dia15/dia15.py
@@ -0,0 +1,31 @@
+#1. Open you python interactive shell and try all the examples covered in this section.
+#errores
+#Syntaxerror
+print(ya me rindo 
+      
+#Nombre
+print(soy muy bueno)
+
+#IndexError
+numero=[1,2,3,4,5]
+print(numero[13])
+
+#ModuleNotFoundError
+import FIFA
+
+#AttributeError
+import FUT
+FUT.add
+
+#KeyError
+niños['gente': 20, 'raza' :5]
+print(niños[locos])
+
+#TypeError
+print('roberto' + 10)
+
+#ValueError
+print(int(roberto))
+
+#ImportError
+suma=3-4
